5c6b177b8dcb8229e55d494662ce82db72db5f57
David Sreesangkom <dsreesa@gmail.com>
2021-04-06 18:07:15 -0400

    add 404

--- authentication/urls.py
+++ authentication/urls.py
@@ -5,5 +5,7 @@ from authentication import views
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('signup/', views.signup_view, name='signup'),
-    path('logout/', views.logout_view, name='logout')
+    path('logout/', views.logout_view, name='logout'),
+    path('', views.index_view, name='index'),
 ]
+# handler404 = 'net.views.error_404_view'
