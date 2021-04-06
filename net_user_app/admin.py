5004649ad5650f08e4feecdeec230854cf47f3e0
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-06 15:33:23 -0400

    Removed duplicate register of Net

--- net_user_app/admin.py
+++ net_user_app/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
 from net_user_app.models import NetUser
-from net.models import Net
 # Register your models here.
 admin.site.register(NetUser)
-admin.site.register(Net)
