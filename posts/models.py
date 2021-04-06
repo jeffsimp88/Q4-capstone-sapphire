0ec37bee4544f99e3777bed0ec4cf4074fd8d9a3
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-06 16:08:48 -0400

    Updated NetUser to the AUTH_USER_MODEL

--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 from mptt.models import MPTTModel, TreeForeignKey
 # Create your models here.
 
@@ -18,10 +19,10 @@ class Post(models.Model):
         ("C", "Comment"),
     ]
     post_type = models.CharField(max_length=1, choices=POST_CHOICES)
-    timestamp = models.DateTimeField(timezone.now)
+    timestamp = models.DateTimeField(default=timezone.now)
     content = models.TextField(max_length=1000, null=False, blank=False)
     author = models.ForeignKey(
-        NetUser,
+        settings.AUTH_USER_MODEL,
         related_name="author",
         on_delete=models.CASCADE)
     upvotes = models.IntegerField(default=0)
