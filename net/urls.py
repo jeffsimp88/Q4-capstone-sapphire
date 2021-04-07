49e04a8deed531cebf3240847f22af022caca44e
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 15:51:10 -0400

    modified subscribe url path

--- net/urls.py
+++ net/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from net.views import index_view, individual_net_view, net_main_view
-from net_user_app.views import subscribe_net
-
+from net import views
 
 urlpatterns = [
-    path('', index_view, name='Homepage'),
-    path('nets/<str:net_title>/', individual_net_view),   
-    path('newnet/', net_main_view, name='new_net'),
-    path('subscribe/<str:net_title>/<int:post_id>/', subscribe_net)
+    path('', views.index_view, name='Homepage'),
+    path('nets/<str:net_title>/', views.individual_net_view, name="Individual Net Page"),   
+    path('newnet/', views.net_main_view, name='Make Net'),
+    path('subscribe/<str:net_title>/', views.subscribe_net, name="Subscribe"),
 ]
