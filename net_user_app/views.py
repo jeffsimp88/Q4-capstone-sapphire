0f8e634b2bde90710dafa4c0eeed8a2dc0210470
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 16:17:04 -0400

    Created view for user profile

--- net_user_app/views.py
+++ net_user_app/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render, redirect
+from net_user_app.models import NetUser
+
+def profile_view(request, username):
+    context = {}
+    page_user = NetUser.objects.get(username=username)
+    print(page_user)
+    context.update({"user": page_user})
+    return render(request, 'profile.html', context)
 
     
