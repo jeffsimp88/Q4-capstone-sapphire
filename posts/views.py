8d317cf1dc02c50d21574947129f85e33007ce53
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 10:36:37 -0400

    Added a post_comment_view

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
-from posts.forms import PostForm
+from posts.forms import PostForm, CommentForm
 from posts.models import Post
 
-def post_form_view(request):
+def post_comment_view(request, post_id):
+    post = Post.objects.filter(id=post_id).first()
     if request.method == 'POST':
-        print("It's a post!")
-    form = PostForm()
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            data = form.cleaned_data
+            new_post = Post.objects.create(
+                header = data['header'],
+                author = request.user,
+                post_type = 'Comment',
+                parent = post,
+                subnet = post.subnet
+            )
+    form = CommentForm()
     context = {'form': form}
     return render(request, "forms.html", context)
