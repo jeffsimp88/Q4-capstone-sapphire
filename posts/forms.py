8f62789e93fb535d98646be3850c9609f2bc2756
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 10:36:15 -0400

    Added CommentForm

--- posts/forms.py
+++ posts/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class PostForm(forms.Form):
     header = forms.CharField(max_length=50)
     content = forms.CharField(widget=forms.Textarea, required=False)
+
+class CommentForm(forms.Form):
+    header = forms.CharField(max_length=50, label="Comment")
