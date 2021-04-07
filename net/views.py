47125334a28f50000fff41ccd432376d8a3864c6
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 15:50:56 -0400

    Added subscribe view, check_subscribe helper func

--- net/views.py
+++ net/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render, redirect
 from net.forms import Create_Net
 from net.models import Net
+from net_user_app.models import NetUser
 from posts.models import Post
+
+
+def check_subscribe(request, net_title):
+    net_info = Net.objects.get(title=net_title)
+    current_user = request.user
+    subscribes= current_user.subs
+    if subscribes.filter(title=net_title).exists():
+        is_subscribed=True
+    else:
+        is_subscribed=False
+    return is_subscribed
+
 
 def net_main_view(request):
     if request.method == 'POST':
@@ -30,7 +43,29 @@ def index_view(request):
 
 def individual_net_view(request, net_title):
     selected_net = Net.objects.filter(title=net_title).first()
+    is_subscribed = check_subscribe(request, selected_net)
     user_subs = request.user.subs.all()
     posts = Post.objects.filter(subnet=selected_net)
-    context = {'net': selected_net, 'posts': posts,'subs': user_subs}
+    context = {
+        'net': selected_net,
+        'is_subscribed': is_subscribed,
+        'posts': posts,
+        'subs': user_subs,
+        }
     return render(request, 'individual_nets.html', context)
+
+def subscribe_net(request, net_title):
+    current_user = request.user
+    current_net = Net.objects.get(title=net_title)
+    check_sub = current_user.subs
+    is_subscribed = False
+    if check_sub.filter(title=current_net).exists():
+        print("You are now unsubscribed")
+        check_sub.remove(current_net)
+        is_subscribed = False
+        return redirect(f'/nets/{net_title}/')
+    else:
+        check_sub.add(current_net)
+        is_subscribed = True
+        return redirect(f'/nets/{net_title}/')
+
