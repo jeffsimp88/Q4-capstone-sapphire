41bd52ae388896adf2d069b1f33863ab6d62f679
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-07 15:46:10 -0400

    Moved subscribe_net to net/views.py

--- net_user_app/views.py
+++ net_user_app/views.py
@@ -1,12 +1,3 @@
 from django.shortcuts import render, redirect
-from net.models import Net
 
-
-def subscribe_net(request, net_title, post_id):
-    current_user = request.user
-    current_net = Net.objects.filter(title=net_title).first()
-    current_user.subs.add(current_net)
-    current_user.save()
-    return redirect(f'/nets/{net_title}/')
-    # return redirect('/')
     
