81d5138fc0688722ddd5a2e42979b43cfd612d60
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-08 12:52:01 -0400

    Created change_theme view

--- net_user_app/views.py
+++ net_user_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, HttpResponseRedirect
 from net_user_app.models import NetUser
 
 def profile_view(request, username):
@@ -7,4 +7,14 @@ def profile_view(request, username):
     context.update({"user": page_user})
     return render(request, 'profile.html', context)
 
-    
+def change_theme(request):
+    current_user = request.user
+    if current_user.site_theme == "Light":
+        current_user.site_theme = 'Dark'
+        current_user.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+    else:
+        current_user.site_theme = 'Light'
+        current_user.save()
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
+
