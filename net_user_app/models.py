8fa87b0258c7ad957cbf168ac6b6775c664e69c1
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-08 12:10:46 -0400

    Added CSS choice field

--- net_user_app/models.py
+++ net_user_app/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 from net.models import Net
 
+LIGHT_DARK = (('Light', 'Light'), ('Dark', 'Dark'))
 
 class NetUser(AbstractUser):
     email = models.EmailField(max_length=100)
@@ -9,6 +10,7 @@ class NetUser(AbstractUser):
     score = models.IntegerField(default= 0)
     subs = models.ManyToManyField(Net, related_name="subs", blank=True)
     followers = models.ManyToManyField('NetUser')
+    site_theme = models.CharField(choices=LIGHT_DARK, max_length=10, default='Light', null=True, blank=True)
     
 
 
