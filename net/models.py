4c2bfcbdf974501c88423a736aafd9cc9ccfe70c
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-09 15:30:08 -0400

    Added unique argument to Net title field

--- net/models.py
+++ net/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from django.conf import settings
 
 class Net(models.Model):
-    title = models.CharField(max_length=50, null=False, blank=False)
+    title = models.CharField(max_length=50, null=False, blank=False, unique=True)
     description = models.TextField(null=False, blank=False)
     rules = models.TextField(null=True, blank=True)
     moderators = models.ManyToManyField(
