6ac55a2c5a70534c9d3306b1b21850d55c7c0f04
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-09 11:14:01 -0400

    Added url path for update users

--- net_user_app/urls.py
+++ net_user_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('users/<username>/', views.profile_view, name="User Profile"),
     path('changetheme/', views.change_theme, name="Change Theme"),
     path('follow/<username>/', views.follow_user, name="Follow User"),
+    path('updateuser/<int:user_id>/', views.update_user, name="Update User"),
 ]
