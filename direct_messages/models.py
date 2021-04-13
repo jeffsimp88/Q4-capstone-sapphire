e65802cc427c9aba24194cd01ff9a529cf5b28df
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-13 09:33:39 -0400

    Added on_delete to fields and imported timezone

--- direct_messages/models.py
+++ direct_messages/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from net_user_app.models import NetUser
+from django.utils import timezone
 
 # Create your models here.
 
 
 class DirectMessage(models.Model):
-    sender = models.ForeignKey(NetUser, related_name="sender")
-    reciever = models.ForeignKey(NetUser, related_name="reciever")
-    msg_content = models.TextField(max_length=1000, related_name="content")
-    created_at = models.TimeField(auto_now=True, auto_now_add=True)
+    sender = models.ForeignKey(NetUser, related_name="sender", on_delete=models.CASCADE)
+    reciever = models.ForeignKey(NetUser, related_name="reciever", on_delete=models.CASCADE)
+    msg_content = models.TextField(max_length=1000,)
+    created_at = models.DateTimeField(default=timezone.now)
