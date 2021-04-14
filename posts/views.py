497edc5a49a08b53e262237bb14ca7193abb015c
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-14 14:27:39 -0400

    Added a form for posting a comment

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
 from posts.forms import PostForm, CommentForm, PostImage
 from posts.models import Post
 from net.models import Net
@@ -6,10 +7,26 @@ from net.models import Net
 def individual_post_view(request, post_id):
     context = {'header': "Post Details"}
     post = Post.objects.get(id=post_id)
-    comments = Post.objects.filter(parent=post)
-    context.update({'post': post, 'comments': comments})
+    comments = Post.objects.filter(parent=post).order_by('timestamp')
+    root_post = post.get_root()
+    if request.method == 'POST':
+        comment_form = CommentForm(request.POST)
+        if comment_form.is_valid():
+            data = comment_form.cleaned_data
+            new_post = Post.objects.create(
+                header = data['header'],
+                author = request.user,
+                post_type = 'Comment',
+                parent = post,
+                subnet = post.subnet
+            )
+            return redirect(f"/posts/{root_post.id}/")
+    comment_form = CommentForm()
+    comment_form.fields['header'].label = ""
+    context.update({'post': post, 'comments': comments, 'comment_form': comment_form})
     return render(request, 'individual_posts.html', context)
 
+@login_required
 def create_post_view(request, net_title):
     context = {'header': "Post a Post"}
     if request.method == 'POST':
@@ -29,6 +46,7 @@ def create_post_view(request, net_title):
     context.update({'form':form})
     return render(request, 'forms.html', context)
 
+@login_required
 def post_image_view(request, net_title):
     context = {'header': "Post a Post"}
     user = request.user
@@ -50,7 +68,7 @@ def post_image_view(request, net_title):
     context.update({'form':form})
     return render(request, 'forms.html', context)
 
-
+@login_required
 def post_comment_view(request, post_id):
     post = Post.objects.filter(id=post_id).first()
     root_post = post.get_root()
@@ -70,13 +88,14 @@ def post_comment_view(request, post_id):
     context = {'form': form}
     return render(request, "forms.html", context)
 
+@login_required
 def upvotes_view(request, post_id):
     post = Post.objects.filter(id=post_id).first()
     post.upvotes +=1
     post.save()
     return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
-
+@login_required
 def downvotes_view(request, post_id):
     post = Post.objects.filter(id=post_id).first()
     post.downvotes +=1
