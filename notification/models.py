5ae410cf5b8c22255437a1c7cb0e892a8d41db59
David Sreesangkom <dsreesa@gmail.com>
2021-04-14 12:10:32 -0400

    show notification button

--- notification/models.py
+++ notification/models.py
@@ -1,14 +1,27 @@
+# from django.contrib.auth.models import User
 from django.db import models
-from direct_messages.models import DirectMessage
-from net_user_app.models import NetUser
+from django.conf import settings
+User = settings.AUTH_USER_MODEL
+# from direct_messages.models import DirectMessage
+# from net_user_app.models import NetUser
 
 # Create your models here.
 class Notification(models.Model):
-    notify = models.ForeignKey(DirectMessage, on_delete=models.CASCADE)
-    notified_user = models.ForeignKey(NetUser, on_delete=models.CASCADE)
-    viewed = models.BooleanField(
-        default=False
+    MESSAGE = 'message'
+    APPLICATION = 'application'
+
+    CHOICES = (
+        (MESSAGE, "Message"),
+        (APPLICATION, "Application"),
     )
 
-    def __str__(self):
-        return "Notification about " + self.notify.notify
+    to_user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
+    notification_type = models.CharField(max_length=20, choices=CHOICES)
+    is_read = models.BooleanField(default=False)
+    extra_id = models.IntegerField(null=True, blank=True)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(User, related_name='creatednotifications', on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
