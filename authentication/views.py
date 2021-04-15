3670c4c13c9f74aa234e46c0eaa6d7bfed6182e8
Donny McClain <donald.mcclain92@gmail.com>
2021-04-15 18:31:57 -0400

    google login finally working, still need to integrate into project

--- authentication/views.py
+++ authentication/views.py
@@ -31,9 +31,12 @@ class SignupView(CreateView):
         if form.is_valid():
             data = form.cleaned_data
             if NetUser.objects.filter(username=data['username']).exists():
-                messages.warning(request, f"Sorry, username {data['username']} already exists.")
+                messages.warning(
+                    req,
+                    f"Sorry, username {data['username']} already exists."
+                    )
                 return redirect('/signup/')
-            new_user = NetUser.objects.create_user(
+            user = NetUser.objects.create_user(
                 username=data['username'],
                 email=data['email'],
                 password=data['password']
@@ -46,31 +49,6 @@ class SignupView(CreateView):
                 login(req, user)
                 return HttpResponseRedirect(req.GET.get('next', '/'))
         return HttpResponseRedirect(req.GET.get('next', '/'))
-
-
-# def signup_view(request):
-#     if request.method == 'POST':
-#         form = SignupForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             new_user = NetUser.objects.create_user(
-#                 username=data['username'],
-#                 email = data['email'],
-#                 password=data['password']
-#             )
-#             user = authenticate(
-#                 request, username=data['username'], password=data['password']
-#             )
-#             if user:
-#                 login(request, user)
-#                 return HttpResponseRedirect(request.GET.get('next', '/'))
-#     form = SignupForm()
-#     context ={
-#         'form': form,
-#         'heading': "Signup as a User",
-#         'signing_in': True,
-#     }
-#     return render(request, "forms.html", context)
 
 
 class LoginView(FormView):
@@ -100,34 +78,8 @@ class LoginView(FormView):
                 return HttpResponseRedirect(req.GET.get('next', '/'))
 
 
-# def login_view(request):
-#     if request.method == 'POST':
-#         form = LoginForm(request.POST)
-#         if form.is_valid():
-#             data = form.cleaned_data
-#             user = authenticate(
-#                 request,
-#                 username=data['username'],
-#                 password=data['password'],
-#             )
-#             if user:
-#                 login(request, user)
-#                 return HttpResponseRedirect(request.GET.get('next', '/'))
-#     form = LoginForm()
-#     context = {
-#         'form': form,
-#         'heading': "Login as a User",
-#         'logging_in': True,
-#     }
-#     return render(request, 'forms.html', context)
-
 class LogoutView(View):
     @method_decorator(login_required)
     def get(self, req):
         logout(req)
         return HttpResponseRedirect('/')
-
-
-# def logout_view(request):
-#     logout(request)
-#     return HttpResponseRedirect('/')
--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -17,9 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import TemplateView
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name='google_login/index.html')),
     path('admin/', admin.site.urls),
+    path('accounts/', include('allauth.urls')),
     path('', include('authentication.urls')),
     path('', include('net_user_app.urls')),
     path('', include('net.urls')),
