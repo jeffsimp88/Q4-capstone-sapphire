0ebb69c1e443f6844daf5bc299cef112d9c83490
Donny McClain <donald.mcclain92@gmail.com>
2021-04-15 18:41:27 -0400

    google login working on both login and signup page

--- reddit_clone/urls.py
+++ reddit_clone/urls.py
@@ -17,10 +17,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='google_login/index.html')),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('authentication.urls')),
