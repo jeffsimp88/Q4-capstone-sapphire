63de687121591a50d35a4323089b42fd5542b589
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-17 12:50:11 -0400

    Updated context processor

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -85,7 +85,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                'notification.context_processors.notifications'
+                'notification.context_processors.get_notifications'
             ],
         },
     },
