5b244a1678ff4ebb7c4626a7066b7bb7f58183e0
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-17 11:21:05 -0400

    Added subnet field, updated choices names

--- notification/models.py
+++ notification/models.py
@@ -2,20 +2,21 @@ from django.db import models
 from django.conf import settings
 User = settings.AUTH_USER_MODEL
 from posts.models import Post
+from net.models import Net
 
 
 class Notification(models.Model):
 
     CHOICES = (
-        ('MESSAGE', 'Message'),
-        ('APPLICATION', 'Application')
+        ('Post', 'Post'),
+        ('Subnet', 'Subnet')
     )
 
     to_user = models.ForeignKey(User, related_name='notifications', on_delete=models.CASCADE)
-    notification_type = models.CharField(max_length=20, choices=CHOICES, default='MESSAGE')
+    notification_type = models.CharField(max_length=20, choices=CHOICES, default='Post')
     is_read = models.BooleanField(default=False)
-    post_comment = models.ForeignKey(Post, related_name='post', on_delete=models.CASCADE, null=True)
-
+    post_comment = models.ForeignKey(Post, related_name='notified_post', on_delete=models.CASCADE, null=True, blank=True)
+    subnet = models.ForeignKey(Net, related_name='notified_subnet', on_delete=models.CASCADE, null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     created_by = models.ForeignKey(User, related_name='creatednotifications', on_delete=models.CASCADE)
 
