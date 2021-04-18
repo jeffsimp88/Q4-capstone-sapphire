3951f3203d1a892d4d3ef02080294eed48c8049a
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-18 15:34:35 -0400

    Created custom admin model for notifs

--- notification/admin.py
+++ notification/admin.py
@@ -1,6 +1,43 @@
 from django.contrib import admin
-
-# Register your models here.
 from .models import Notification
+from notification.forms import CreateNotification
 
-admin.site.register(Notification)
+
+
+def mark_read(modeladmin, request, queryset):
+    queryset.update(is_read=True)
+mark_read.short_description = "Mark selected as read"
+
+def mark_unread(modeladmin, request, queryset):
+    queryset.update(is_read=False)
+mark_unread.short_description = "Mark selected as unread"
+
+
+class NotificationAdmin(admin.ModelAdmin):
+    add_form = CreateNotification
+    model = Notification
+    list_display = (
+        'to_user',
+        'notification_type',
+        'is_read',
+        'post_comment',
+        'subnet',
+        'created_at',
+        'created_by',
+        )
+
+    fieldsets = (
+        (None, {
+            "fields": list_display,
+        }),
+    )
+    
+    add_fieldsets = (
+        (None, {
+            'classes':('wide'), 
+            'fields': list_display,
+        },),)
+    ordering = list_display
+    actions = [mark_read, mark_unread,]
+
+admin.site.register(Notification, NotificationAdmin)
