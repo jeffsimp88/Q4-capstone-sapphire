f9271e97525bd6b5df3edd776e6caae86998b80c
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-18 15:31:56 -0400

    Created list of users with messages

--- direct_messages/views.py
+++ direct_messages/views.py
@@ -4,12 +4,18 @@ from itertools import chain
 from direct_messages.models import DirectMessage
 from direct_messages.forms import DirectMessageForm
 from net_user_app.models import NetUser
+from notification.models import Notification
 
 @login_required
 def message_view(request):
     messages = DirectMessage.objects.filter(reciever=request.user)
     sent_messages = DirectMessage.objects.filter(sender=request.user)
-    context = {'messages': messages, 'sent_messages': sent_messages}
+    user_list=[]
+    for message in sent_messages:
+        user_list.append(message.reciever)
+    for message in messages:
+        user_list.append(message.sender)
+    context = {'users': set(user_list)}
     return render(request, 'messages.html', context)
 
 @login_required
@@ -30,9 +36,14 @@ def user_message_view(request, username):
                 reciever = reciever_user,
                 msg_content = data['msg_content'],
             )
+            Notification.objects.create(
+                to_user=reciever_user,
+                notification_type="Message",
+                created_by=request.user,
+            )
             return HttpResponseRedirect(f'/messages/{reciever_user.username}/')
     message_form = DirectMessageForm()
-    context={'messages': messages, 'message_form':message_form}
+    context={'messages': messages, 'recieving_user': reciever_user}
     return render(request, 'user_messages.html', context)
 
 
@@ -47,6 +58,11 @@ def create_message(request, username):
                 sender = request.user,
                 reciever = reciever_user,
                 msg_content = data['msg_content'],
+            )            
+            Notification.objects.create(
+                to_user=reciever_user,
+                notification_type="Message",
+                created_by=request.user,
             )
             return HttpResponseRedirect(f'/users/{reciever_user.username}/')
 
