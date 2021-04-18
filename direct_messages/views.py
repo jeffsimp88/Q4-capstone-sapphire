27dfc88f9e951b0b244abefa79fc9616695cb496
jeffsimp88 <jdsimpson806@gmail.com>
2021-04-18 08:59:59 -0400

    Added a login required decorator

--- direct_messages/views.py
+++ direct_messages/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 from direct_messages.models import DirectMessage
 from direct_messages.forms import DirectMessageForm
 
+@login_required
 def message_view(request):
     messages = DirectMessage.objects.filter(reciever=request.user)
 
