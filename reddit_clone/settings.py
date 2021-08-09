bc7dbaed62f0be59e672989b103869eeb6f99e3f
Mike Hartley <mhartley19@gmail.com>
2021-08-09 13:36:58 -0500

    trying to deploy with vercel

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django.core'
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
